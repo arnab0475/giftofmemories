1076ffb70b9c5f9249ff1b13f1e22dcfd0bbc2c1
Niladri080 <nilmandal080@gmail.com>
2026-02-06 14:10:55 +0530

    credentials updated and ui bugs fixed

--- client/src/pages/AdminPageHeroes.jsx
+++ client/src/pages/AdminPageHeroes.jsx
@@ -172,9 +172,9 @@ const AdminPageHeroes = () => {
   };
 
   return (
-    <div className="min-h-screen bg-gray-50 flex">
+    <div className="flex min-h-screen bg-[#FAF9F6] font-inter">
       <Sidebar />
-      <div className="flex-1 flex flex-col">
+      <div className="flex-1 ml-[260px] flex flex-col min-h-screen">
         <TopBar title="Page Hero Management" />
 
         <main className="flex-1 p-6 overflow-auto">
